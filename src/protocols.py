7b4de5048de44ceb8fcfd7a599af9273481737fc
Makar <krikazon@mail.ru>
2024-05-30 19:52:14 +0700

    positionable protocol added

---
+++ src/protocols.py
@@ -0,0 +1,9 @@
+from typing import Protocol
+
+from src.helper.vec import Vec2
+
+
+class Positionable(Protocol):
+    @property
+    def position(self):
+        pass
--- src/widgets/label.py
+++ src/widgets/label.py
@@ -1,17 +1,25 @@
-from typing import List
+from typing import List, Union
 
 import pyxel as px
 
 from src.helper.vec import Vec2
+from src.protocols import Positionable
 
 
 class Label:
-    def __init__(self, position: Vec2, lines: List[str], color: int, relative_to=None):
+    def __init__(self, position: Vec2, lines: List[str], color: int, relative_to: Union[Positionable, None] = None):
         self.__position = position
         self.__lines = lines
         self.__color = color
         self.__relative_to = relative_to
         self.__visible = True
+
+    @property
+    def position(self):
+        if self.__relative_to is None:
+            return self.__position
+        else:
+            return self.__relative_to.position + self.__position
 
     def add_line(self, line: str):
         self.__lines.append(line)
